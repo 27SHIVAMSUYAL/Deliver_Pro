e647902c35f65c9561340985a6eb15d6d1aa424e
shivam <27shivam.1o1@gmail.com>
2025-02-02 20:38:23 +0530

    this is the first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import NAVBAR from "./components/navbar";
+function App() {
+  return (
+    <>
+      <NAVBAR />
+      
+
+
+
+    </>
+  );
+}
+export default App;
+
+
